56ab9b77e89d46bc8f8c7495ca292b7e28f62251
martijnverleg <martijn.verleg@gmail.com>
2024-07-15 21:37:55 +0200

    setup is now the home page

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,7 @@
     <nav class="container">
       <div class="row">
         <div class="col-12 d-flex flex-row gap-2">
-          <RouterLink class="router-link nav__item" to="/">Home</RouterLink>
-          <RouterLink class="router-link nav__item" to="/setup">Setup</RouterLink>
+          <RouterLink class="router-link nav__item" to="/">Setup</RouterLink>
           <RouterLink class="router-link nav__item" to="/roll">Roll</RouterLink>
         </div>
       </div>
@@ -39,5 +38,4 @@ loadStateFromLocalStorage()
 .nav__item {
   color: yellow;
 }
-
 </style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import SetupView from '../views/SetupView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'setup',
+      component: SetupView
     },
     {
       path: '/roll',
       name: 'roll',
       component: () => import('../views/RollView.vue')
-    },
-    {
-      path: '/setup',
-      name: 'setup',
-      component: () => import('../views/SetupView.vue')
     }
   ]
 })
