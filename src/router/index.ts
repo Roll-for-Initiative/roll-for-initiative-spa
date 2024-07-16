dbdc9817585e53ad53866982fbb621c7409f1c6d
martijnverleg <martijn.verleg@gmail.com>
2024-07-16 20:42:58 +0200

    removed async route view

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import SetupView from '../views/SetupView.vue'
+import RollView from '../views/RollView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -12,7 +13,7 @@ const router = createRouter({
     {
       path: '/roll',
       name: 'roll',
-      component: () => import('../views/RollView.vue')
+      component: RollView
     }
   ]
 })
