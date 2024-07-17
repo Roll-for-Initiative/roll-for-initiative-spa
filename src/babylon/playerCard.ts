90ab558c28de98f49d87a52499744fa666fd99bb
marlonsijnesael <marlon@Marlons-MBP.home>
2024-07-17 21:26:30 +0200

    couple playerdata to player image

--- src/babylon/playerCard.ts
+++ src/babylon/playerCard.ts
@@ -25,7 +25,6 @@ type cardParts = {
 const IMAGES = [
     "dragon.jpg",
     "dwarf.jpg",
-    "jelle.jpg",
     "wizard.jpg"
 ]
 
@@ -53,12 +52,7 @@ export class PlayerCard {
     }
 
     async loadModel() {
-        const pathParts = modelUrl.split('/')
-        const fileName = pathParts.pop()
-        const path = pathParts.join("/")
-        console.log(`${path}${fileName}`)
         const model = await BABYLON.SceneLoader.ImportMeshAsync("", "src/assets/models/card.glb", "", this.scene)
-        console.log("meshe", model.meshes[0].getChildren())
 
         this.parts = {
             backdrop: model.meshes[0].getChildMeshes(false, (mesh) => mesh.name === "backdrop")[0],
@@ -67,69 +61,39 @@ export class PlayerCard {
             meta: model.meshes[0].getChildMeshes(false, (mesh) => mesh.name === "meta")[0],
             picture: model.meshes[0].getChildMeshes(false, (mesh) => mesh.name === "picture")[0]
         }
-        this.mainMesh = model.meshes[0]
 
+        this.mainMesh = model.meshes[0]
 
         this.parts.initiative.visibility = 0
 
         const backdropMaterial = new BABYLON.StandardMaterial("backdropMaterial", this.scene);
         backdropMaterial.diffuseTexture = new BABYLON.Texture("src/assets/models/textures/wood.jpg", this.scene);
         backdropMaterial.diffuseColor = BABYLON.Color3.FromHexString("#954535");
+        
         this.parts.backdrop.material = backdropMaterial
-        this.dir = 1
-        this.speed = Math.random()
+       
+        // Load material from node material editor
 
+        const nm = await BABYLON.NodeMaterial.ParseFromSnippetAsync("IG38UL#20", this.scene)
 
-        // const pictureMaterial = new BABYLON.StandardMaterial("pictureMaterial", this.scene);
-        // console.log(imgName)
-        // pictureMaterial.diffuseTexture = new BABYLON.Texture(`http://localhost:5173/src/assets/models/textures/${imgName}`, this.scene);
-        // this.parts.picture.material = pictureMaterial
-        // console.log(this.name)
-
-
-        // console.log(myNodeMaterial)
-        const imgName = this.name === "Monsters" ? MONSTERS[Math.floor(Math.random() * MONSTERS.length)] : IMAGES[Math.floor(Math.random() * IMAGES.length)]
-        const nm = await BABYLON.NodeMaterial.ParseFromSnippetAsync("IG38UL#15", this.scene)
-
-        console.log(this.parts.picture)
-        //   console.log('before',  mat.getBlockByPredicate(input => input.name === "pic    ture").texture as)
         const textureBlock = nm.getBlockByPredicate(input => input.name === "picture") as BABYLON.TextureBlock;
-        console.log("texture block", textureBlock)
         const interval = setInterval(()=>{
-            console.log(textureBlock.isReady())
             if (textureBlock.isReady()){
                 const imgName = this.name === "Monsters" ? MONSTERS[Math.floor(Math.random() * MONSTERS.length)] : IMAGES[Math.floor(Math.random() * IMAGES.length)]
-                console.log(imgName)
-                const text = new BABYLON.Texture(`src/assets/models/textures/${imgName}`, this.scene);
-                console.log(text)
+                const text = new BABYLON.Texture(this.imgUrl ? this.imgUrl : `src/assets/models/textures/${imgName}`, this.scene);
                 textureBlock.texture = text
                 clearInterval(interval)
-                textureBlock.visibleOnFrame = true
-                console.log('texture block',textureBlock.texture)
-
-
             }
         }, 200)
+
         this.parts.picture.material = nm
-        
-
-        //   mat.getBlockByPredicate(input => input.name === "picture").source = 
-        //   console.log('after',  mat.getBlockByPredicate(input => input.name === "picture").texture)
-
-        // });
-
 
         const font = "82px RuneScape Chat";
-
-
-
-
         const textureName = new BABYLON.DynamicTexture("nameTexture", { width: 400, height: 100 }, this.scene);
         const materialName = new BABYLON.StandardMaterial("nameMaterial", this.scene);
         materialName.diffuseTexture = textureName;
         textureName.drawText(this.name, 40, 80, font, "yellow", "#0101", false, true);
         this.parts.name.material = materialName
-
 
         const textureInitiative = new BABYLON.DynamicTexture("initiativeTexture", { width: 400, height: 100 }, this.scene);
         const materialInitiative = new BABYLON.StandardMaterial("initiativeMaterial", this.scene);
@@ -141,28 +105,22 @@ export class PlayerCard {
         model.meshes[0].rotate(new BABYLON.Vector3(0, 0, 1), -Math.PI / 2, BABYLON.Space.LOCAL)
         model.meshes[0].rotate(new BABYLON.Vector3(0, 1, 0), -Math.PI, BABYLON.Space.WORLD)
 
-
         this.parent.addChild(model.meshes[0])
         model.meshes[0].position._z = this.xPos * 5
 
         this.startPos = this.mainMesh.position
-        const moveDistance = new BABYLON.Vector3(0,5,0)
+        const moveDistance = new BABYLON.Vector3(0,2.5,0)
 
         this.endPos = this.mainMesh.position.add(BABYLON.Vector3.Up().multiply(moveDistance))
 
+        this.dir = 1
+        this.speed = Math.random()
     }
     
     idleLerp(){
-        // const amplitude = 1
-        // const period = 1000000
-        // const theta = BABYLON.Tools.Now / period
-        // const distance = amplitude * Math.sin(theta)
-        // const scalar = BABYLON.Vector3.Up().multiplyByFloats(0, distance, 0)
-        // this.mainMesh.position = this.startPos.addInPlace(scalar)
         let t = BABYLON.Tools.Now / 10000 * this.speed;
         t = Math.sin(t * Math.PI * 0.5);
         this.mainMesh.position = BABYLON.Vector3.Lerp(this.startPos, this.endPos, t);
-
     }
 
     async reveal() {
@@ -190,5 +148,4 @@ export class PlayerCard {
         }
         this.mainMesh.position.y +=  this.speed * this.dir
     }
-
 }
